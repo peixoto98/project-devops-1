78ea5ae9a89e96e872201bbb8c028146d9a36d1a
peixoto98 <peixoto.rafael98@gmail.com>
2025-01-23 17:52:42 -0300

    Add: terraform data key pair and terraform apply

--- main.tf
+++ main.tf
@@ -2,16 +2,20 @@ provider "aws" {
   region = local.region
 }
 
-resource "aws_key_pair" "id_rsa_public" {
-  key_name   = local.key_name
-  public_key = file("~/.ssh/id_ed25519.pub")
+# resource "aws_key_pair" "id_rsa_public" {
+#   key_name   = local.key_name
+#   public_key = file("~/.ssh/id_ed25519.pub")
+# }
+
+data "aws_key_pair" "wsl" {
+  key_name = "wsl"
 }
 
 resource "aws_instance" "postgresql" {
   ami           = local.ami
   count         = local.count
   instance_type = local.instance_type
-  key_name      = local.key_name
+  key_name      = data.aws_key_pair.wsl.key_name
 
   tags = local.tags
 }
